58ec673daa8bb30c460b0e8964c13f16869e98ac
karthik <karthikajar555@gmail.com>
2021-12-09 00:40:28 +0530

    - project page, query variable

--- src/pageTemplate/index.jsx
+++ src/pageTemplate/index.jsx
@@ -1,20 +1,45 @@
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
 import Layout from "../components/Layout"
-import * as styles from '../styles/pageTemplate.module.css'
+import * as styles from "../styles/pageTemplate.module.css"
 
-const PageTemplate = () => {
+const PageTemplate = ({ data }) => {
+  const { html } = data.markdownRemark
+  const { date, stack, title, featuredImg } = data.markdownRemark.frontmatter
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-          {/* <Img fluid={} /> */}
+          {<GatsbyImage image={getImage(featuredImg)} alt={title}/>}
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <div
+          className={styles.html}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </Layout>
   )
 }
- 
+
 export default PageTemplate
+
+export const query = graphql`
+  query ProjectDetail($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        date
+        stack
+        title
+      }
+    }
+  }
+`
